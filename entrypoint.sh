5d1798aaea2e8c64969260256205ab5f3a1b568f
Gregor Grote <grote@riege.com>
2021-08-17 15:33:39 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,22 +8,3 @@ dir=${INPUT_DIR:-$1}
 
 >&2 echo $dir
 
-# if [ -n "${INPUT_STRICT:-}" ]
-# then
-#   strict='--strict'
-# fi
-
-# if [ ! -d $dir ]
-# then
-#   >&2 echo "Target directory does not exist: ${dir}"
-#   exit 1
-# fi
-
-# for d in ${dir}/* 
-# do
-#   if [ -d ${d} -a -f ${d}/schema.yaml -a -f ${d}/data.yaml] then
-#     >&2 echo ${d}
-#     #yamale --schema=$d/schema.yaml $d/data.yaml $strict;
-#   fi
-# done
-
